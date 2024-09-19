c0c786ba8368e64d1a4070df5f010794ef11b913
Ron Tseytlin <ronts390@gmail.com>
2024-09-20 01:08:38 +0300

    added history preview

--- src/components/TrackerFullscreen.vue
+++ src/components/TrackerFullscreen.vue
@@ -1,10 +1,11 @@
 <script setup lang="ts">
 import { useTrackerDurationComponents } from '@/composables/trackerTimer'
 import { useTrackersStore, type Tracker } from '@/stores/trackers'
-import ResetSlider from './ResetSlider.vue'
-import IconTrophy from './icons/IconTrophy.vue'
-import { computed } from 'vue'
 import { getDurationString } from '@/util'
+import { computed } from 'vue'
+import ResetSlider from './ResetSlider.vue'
+import IconHistory from './icons/IconHistory.vue'
+import IconTrophy from './icons/IconTrophy.vue'
 
 const store = useTrackersStore()
 
@@ -15,6 +16,22 @@ const props = defineProps<{
 const { days, hours, minutes, seconds, restartTimer } = useTrackerDurationComponents(props.tracker)
 
 const best = computed(() => store.trackerBests.get(props.tracker))
+
+const numHistoryPreviewEntries = 3
+
+const history = computed(() => {
+  const entries = []
+  const start = Math.max(props.tracker.resets.length - numHistoryPreviewEntries, 0)
+  for (let i = start; i < props.tracker.resets.length; i++) {
+    entries.push({
+      index: i,
+      date: props.tracker.resets[i],
+      duration:
+        props.tracker.resets[i] - (i > 0 ? props.tracker.resets[i - 1] : props.tracker.dateCreated)
+    })
+  }
+  return entries
+})
 
 const resetTracker = () => {
   store.resetTracker(props.tracker)
@@ -60,7 +77,7 @@ const resetTracker = () => {
         <div class="name">{{ tracker.name }}</div>
       </div>
     </div>
-    <div class="best-frame" v-if="best">
+    <div class="info-frame best" v-if="best">
       <div class="frame-header">
         <IconTrophy class="icon" />
         Best
@@ -68,6 +85,19 @@ const resetTracker = () => {
       <div class="content">
         <div class="timer-font">{{ getDurationString(best.duration) }}</div>
         Achieved on {{ new Date(best.date).toLocaleDateString() }}
+      </div>
+    </div>
+    <div class="info-frame history" v-if="tracker.resets.length > 0">
+      <div class="frame-header">
+        <IconHistory class="icon" />
+        History
+      </div>
+      <div class="content">
+        <div class="history-entry" v-for="entry in history" :key="entry.date">
+          <div class="index">{{ entry.index + 1 }}</div>
+          <div>{{ new Date(entry.date).toLocaleDateString() }}</div>
+          <div class="timer-font">{{ getDurationString(entry.duration) }}</div>
+        </div>
       </div>
     </div>
     <div class="slider-container">
@@ -97,7 +127,6 @@ const resetTracker = () => {
 .timer-font {
   font-family: monospace;
   line-height: 1;
-  text-align: center;
 }
 
 .timer-frame {
@@ -132,29 +161,64 @@ const resetTracker = () => {
   font-size: 200%;
 }
 
-.best-frame {
+.info-frame {
   margin-top: var(--margin-l);
   margin-left: var(--margin-l);
   margin-right: var(--margin-l);
   padding: var(--margin-m);
-  background-color: var(--color-best);
+  background-color: var(--color-frame-background);
   border-radius: var(--radius-l);
   display: flex;
   align-items: center;
   justify-content: space-between;
+  gap: var(--margin-m);
 }
 
-.best-frame .content {
+.info-frame .content {
+  flex-grow: 1;
+}
+
+.info-frame.best {
+  background-color: var(--color-best);
+}
+
+.info-frame.best .content {
   display: flex;
   flex-direction: column;
   align-items: end;
 }
 
-.best-frame .timer-font {
+.info-frame.best .timer-font {
   font-size: 34px;
 }
 
+.info-frame.history .content {
+  display: grid;
+  grid-template-columns: auto 1fr 1fr;
+  grid-template-rows: repeat(v-bind('numHistoryPreviewEntries'), auto);
+  grid-auto-flow: row;
+  column-gap: var(--margin-m);
+  text-align: right;
+}
+
+.history-entry {
+  display: grid;
+  grid-column: 1 / 4;
+  grid-template-columns: subgrid;
+  grid-auto-flow: column;
+  align-items: center;
+}
+
+.history-entry .index {
+  opacity: 0.5;
+}
+
+.history-entry .timer-font {
+  font-size: 1.1em;
+}
+
 .frame-header {
+  width: 54px;
   display: flex;
   flex-direction: column;
   align-items: center;
